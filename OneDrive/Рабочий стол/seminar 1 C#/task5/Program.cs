b77d5b2878cb96ae1aa9ee2535694480f5f98ecf
DmitriyGera <dimmo12397@mail.ru>
2022-02-28 15:15:55 +0300

    Третья задача семинара С#

---
+++ OneDrive/Рабочий стол/seminar 1 C#/task5/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число а => ");
+string inputNumber = Console.ReadLine();
+int numberA = int.Parse(inputNumber);
+
+int x = numberA + 9;
+Console.WriteLine($"Функция x = f(a): {x} ");
