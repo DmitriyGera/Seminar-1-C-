eecb861d005821bdf32986a57e14d847c9c84373
DmitriyGera <dimmo12397@mail.ru>
2022-02-28 16:03:44 +0300

    Четвертая задача семинара С#

---
+++ OneDrive/Рабочий стол/seminar 1 C#/task7/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число => ");
+string inputNumber = Console.ReadLine();
+int N = int.Parse(inputNumber);
+
+int A = 0 - N;
+Console.Write(A+" ");
+while (A < N)
+{
+    A = A + 1;
+    Console.Write(A+" ");
+}
