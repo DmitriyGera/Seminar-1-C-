85aa649fae6ce99ef5cc543652d5138066ea0138
DmitriyGera <dimmo12397@mail.ru>
2022-02-28 17:11:13 +0300

    Первая задача

---
+++ OneDrive/Рабочий стол/DZ seminar 1 C#/Fork/Program.cs
@@ -0,0 +1,13 @@
+﻿double a, b;
+Console.Write("Введите число A = "); 
+string input = Console.ReadLine();
+a = double.Parse(input);
+Console.Write("Введите число B = "); 
+input = Console.ReadLine();
+b = double.Parse(input);
+const string lt = "<", gt = ">";
+if (a < b) 
+input = lt; 
+else 
+input = gt;
+Console.WriteLine($" { a } { input } { b } ");
