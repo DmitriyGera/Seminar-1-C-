bb6388952cf9532c33c6b7a0a0a4fc4f90d6ae6f
DmitriyGera <dimmo12397@mail.ru>
2022-02-28 14:53:43 +0300

    Задачи первого семинара С#

---
+++ OneDrive/Рабочий стол/seminar 1 C#/task0/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число =>");
+string strValue = Console.ReadLine();
+int value = int.Parse(strValue);
+
+int result = value * value;
+Console.WriteLine(result);
---
+++ OneDrive/Рабочий стол/seminar 1 C#/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите значение А => ");
+string inputA = Console.ReadLine();
+int numberA = int.Parse(inputA);
+
+Console.Write("Введите число B => ");
+string inputB = Console.ReadLine();
+int numberB = int.Parse(inputB);
+
+int value = numberA * numberA;
+if (numberB == value) Console.WriteLine("Да");
+else
+{
+    Console.WriteLine("Нет");
+}
