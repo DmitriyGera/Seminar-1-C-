590c97bf61e212b7001c251aa04f8b323f3ae17d
DmitriyGera <dimmo12397@mail.ru>
2022-02-28 16:51:16 +0300

    Первая задача

---
+++ OneDrive/Рабочий стол/DZ seminar 1 C#/Zadacha1/Program.cs
@@ -0,0 +1,14 @@
+﻿double a, b;
+Console.Write("Введите число A = "); 
+string gotcha = Console.ReadLine();
+a = double.Parse(gotcha);
+Console.Write("Введите число B = "); 
+gotcha = Console.ReadLine();
+b = double.Parse(gotcha);
+const string lt = "<", gt = ">";
+if (a < b) 
+gotcha = lt; 
+else 
+gotcha = gt;
+Console.WriteLine($" { a } { gotcha } { b } ");
+
