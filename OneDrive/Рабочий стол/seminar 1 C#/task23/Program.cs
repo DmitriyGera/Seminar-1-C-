fbd515ab760779cc9e7a265a22ee4d630852a5e1
DmitriyGera <dimmo12397@mail.ru>
2022-03-10 16:54:18 +0300

    Семинар 3 C#

---
+++ OneDrive/Рабочий стол/seminar 1 C#/task23/Program.cs
@@ -0,0 +1,17 @@
+﻿int Prompt(string messege)
+{
+    Console.Write(messege);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int x1 = Prompt("Введите х первой точки: ");
+int y1 = Prompt("Введите y первой точки: ");
+int x2 = Prompt("Введите х второй точки: ");
+int y2 = Prompt("Введите y второй точки: ");
+double x3 = x1 - x2;
+double y3 = y1 - y2;
+
+double d = Math.Sqrt(Math.Pow(x3, 2)+ Math.Pow(y3, 2));
+Console.WriteLine(d);
---
+++ OneDrive/Рабочий стол/seminar 1 C#/task25/Program.cs
@@ -0,0 +1,17 @@
+﻿int Prompt(string messege)
+{
+    Console.Write(messege);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int N = Prompt("Введите число: ");
+
+int A = 1;
+Console.Write(A + " ");
+while (A <= N)
+{
+    A = A + 1;
+    Console.Write(A * A + " ");
+}
